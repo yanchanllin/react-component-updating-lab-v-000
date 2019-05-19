9716f1f584a27baad0f3b468d4c8050a26b02e2e
Yan C Lin <yonalin1@gmail.com>
2019-05-19 18:22:53 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -19,7 +19,12 @@ componentDidUpdate(){
   "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 
-
+shouldComponentUpdate(nextProps, nextState) {
+  if (this.state.time === nextState.time) {
+    return false
+  }
+  return true
+}
   componentDidMount() {
     this.interval = setInterval(this.clockTick, this.props.updateInterval*1000)
   }
