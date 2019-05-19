137c1e175b87d37b9bc96b62ef3c7dc84bcc859e
Yan C Lin <yonalin1@gmail.com>
2019-05-19 18:18:54 +0000

    Automatically backed up by Learn

--- src/Timer.js
+++ src/Timer.js
@@ -14,7 +14,10 @@ class Timer extends Component {
 
 
   //Your code here
-
+componentDidUpdate(){
+  this.timer.current.style.color =
+  "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 
   componentDidMount() {
